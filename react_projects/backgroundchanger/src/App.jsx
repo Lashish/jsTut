1f54ae93013e89cf75eb8456442f33fa718dcb52
Lashish <ashishklite@gmail.com>
2023-12-27 02:25:43 +0530

    colorchanger modify

--- react_projects/backgroundchanger/src/App.jsx
+++ react_projects/backgroundchanger/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
 
   return (
-    <>
-      <h1>Background Changer</h1>
-    </>
+
+    <div className='w-screen h-screen duration-200' style={{ backgroundColor: color }}>
+      <button className='bg-red-700' onClick={() => setColor('red')}>red</button>
+      <button className='bg-green-700' onClick={() => setColor('green')}>green</button>
+      <button className='bg-gray-700' onClick={() => setColor('gray')}>gray</button>
+      <button className='bg-white text-black' onClick={() => setColor('white')}>white</button>
+      <button className='bg-black' onClick={() => setColor('black')}>black</button>
+      <button className='bg-purple-700' onClick={() => setColor('purple')}>purple</button>
+      <button className='bg-olive-700' onClick={() => setColor('olive')}>olive</button>
+    </div>
+
   )
 }
 
